c43dec2a65e2c547b2dbf506a9d2c8bff0bbc595
Marlon <marlu_tcu@hotmail.com>
2017-05-15 12:55:00 -0300

    template padrão

---
+++ resources/views/template.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>sga_fatec</title>
+	
+	 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	 <link href="{{ asset('js/app.js') }}" rel="stylesheet">
+
+	 @stack('script')
+	
+</head>
+<body>
+
+@yield('conteudo')
+
+
+</body>
+</html>
