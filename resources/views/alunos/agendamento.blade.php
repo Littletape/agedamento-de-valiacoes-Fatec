b22f953a7d973e0a3ff059fe0ea0465fc11c69e0
Marlon <marlu_tcu@hotmail.com>
2017-05-24 23:57:59 -0300

    implementações

--- resources/views/alunos/agendamento.blade.php
+++ resources/views/alunos/agendamento.blade.php
@@ -11,14 +11,31 @@
 			<div class="panel panel-primary">
 				<div class="panel-heading"><center><b>Semestre XX</b></center></div>
 				<table class="table table-striped">
-					@forelse($semanas as semana)
+					@forelse($semanas as $semana)
+					<th colspan="4">Semana {{$semana->semana}}</th>
+						
+							<tr>
+							@forelse($aval as $avaliacoes)
+							<td>
+							<table class="table">
+									
+									<th>{{$avaliacoes->data}}</th>
+									<tr><td>{{$avaliacoes->dia}}</td></tr>
+									<tr><td>{{$avaliacoes->materia1}}</td></tr>
+									<tr><td>{{$avaliacoes->materia2}}</td></tr>
+									
+							</table>
+							</td>		
+							@empty
+							<h2>Nenhuma avaliação cadastrada até o momento</h2>
+							@endforelse
+							</tr>
+						
 
 					@empty
 					<h2>Nenhuma avaliação cadastrada até o momento</h2>
 					@endforelse
-					<td>
-						<table> </table>
-					</td>
+					
 					
 				</table>
 			</div>	
