f8ba65e6b872f28c0720e29c136cab0047f5d7de
Marlon <marlu_tcu@hotmail.com>
2017-05-24 23:59:14 -0300

    implementações

--- app/model/Avaliacoes.php
+++ app/model/Avaliacoes.php
@@ -20,11 +20,11 @@ class Avaliacoes extends Model
 
     public function avaliacoesAgendadas(){
 
-    	$dados = DB::table('avaliacoes')->select('avaliacoes.id','avaliacoes.data','semestre_avaliacoes.semestre','semanas_avaliacoes.semana','avaliacoes.dia','materias.nome','avaliacoes.pdf_nome','materia2.nome')
+    	$dados = DB::table('avaliacoes')->select('avaliacoes.id','avaliacoes.data','semestre_avaliacoes.semestre','semanas_avaliacoes.semana','avaliacoes.dia','avaliacoes.pdf_nome',DB::raw('(SELECT nome from materias where id = avaliacoes.materia_id) as materia1' ), DB::raw('(SELECT nome from materias where id = avaliacoes.materia2_id) as materia2' ))
     	->join('semestre_avaliacoes','avaliacoes.semestre_id','semestre_avaliacoes.id')
     	->join('semanas_avaliacoes','avaliacoes.semana_id','semanas_avaliacoes.id')
-    	->join('materias','materias.id','avaliacoes.materia_id')
-    	->join(DB::raw('('.DB::select('select Distinct id,nome from materias').') AS materia2 '),'materia2.id','avaliacoes.materia2_id')
+    	
+    	
     	->get();
 
     	if(!empty($dados) ){
