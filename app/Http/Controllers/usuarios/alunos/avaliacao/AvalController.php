4d8eaa037de73fda99bb025746fef74c694cf84f
Marlon <marlu_tcu@hotmail.com>
2017-06-01 23:45:07 -0300

    funções pra listar avaliação e agendar avaliação

--- app/Http/Controllers/usuarios/alunos/avaliacao/AvalController.php
+++ app/Http/Controllers/usuarios/alunos/avaliacao/AvalController.php
@@ -5,17 +5,32 @@ namespace App\Http\Controllers\usuarios\alunos\avaliacao;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\model\AvaliacaoAgendada;
+use App\Model\Avaliacoes;
+use App\Model\Semestre;
+use App\Model\Semana;
 
 class AvalController extends Controller
 {
-    public function agendarAvaliacao($materia_id,$id,$status, AvaliacaoAgendada $avaliacao){
-    	$idUsu = session()->get('id');
-    	
-    	if($status == 'true'){
-    		$avaliacao->create(['avaliacoes_id' => $id, 'usuario_id' => $idUsu, 'materia_id' => $materia_id]);
-    	}else{
-    		$avaliacao->where('avaliacoes_id', $id)->delete();
 
-    	}
-    }
+	public function listarAvaliacoes(Avaliacoes $avaliacoes, Semestre $semestre, Semana $semana){
+
+		$aval = $avaliacoes->avaliacoesCadastradas();
+		$semestres = $semestre->all();
+		$semanas = $semana->all();
+		$idUsu = session()->get('id');
+		
+		// retorna a view de agendamento de avaliacoes
+ 		return view('alunos.agendamento',compact('aval','semestres','semanas','idUsu'));
+	}
+
+	public function agendarAvaliacao($materia_id,$id,$status, AvaliacaoAgendada $avaliacao){
+		$idUsu = session()->get('id');
+
+		if($status == 'true'){
+			$avaliacao->create(['avaliacoes_id' => $id, 'usuario_id' => $idUsu, 'materia_id' => $materia_id]);
+		}else{
+			$avaliacao->where('avaliacoes_id', $id)->delete();
+
+		}
+	}
 }
