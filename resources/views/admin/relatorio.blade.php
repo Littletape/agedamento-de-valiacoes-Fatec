db17e7e99c4aee9c43dfa6f7f01cbf7ea6cc5f15
Anderson Lima <andersoncardosolima0@gmail.com>
2017-06-07 23:12:22 -0300

    Estou mudando a tela de relatorio

--- resources/views/admin/relatorio.blade.php
+++ resources/views/admin/relatorio.blade.php
@@ -3,48 +3,56 @@
 @section("conteudo")
 
 <div class="row">
-	<div class="col-md-6 col-md-offset-3 col-xs-12">
-		
-		<form>
-			<label>Digite os termos que devem aparecer no relatorio:</label>
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Palavras Chaves">
-				<span class="input-group-btn">
-					<button class="btn btn-success" type="button"><span class="glyphicon glyphicon-file"/></button>
-				</span>
+
+	<div class="col-md-4">
+		<div class="panel panel-primary"> <!--style="border-radius:50px"-->
+			<div class="panel panel-heading">Provas agendadas para hoje</div>
+			<div class="panel-body">
+				<h4>Formato: .pdf <img src="images/pdfIco.png"></h4>
+				<h4>Conteudo do arquivo:</h4>
+				<p>Data, Semestre, Materia, Nome do Aluno</p>						
 			</div>
+			<div class="panel-footer">
+				<center><a href="">Download<br><span class="glyphicon glyphicon-download"> <b style="text-align: center;"></span></b></a></center>
+			</div>
+		</div>
+	</div> <!-- Div col-md-4-->
 
-			<center><h1>OU</h1></center>
+	<div class="col-md-4">
+		<div class="panel panel-primary"> <!--style="border-radius:50px"-->
+			<div class="panel panel-heading">Confirmações de presença</div>
+			<div class="panel-body">
+				<h4>Formato: .xls (excel)<img src="images/exelIco.png"></h4>
+				<h4>Conteudo do arquivo:</h4>
+				<p>Data, Semestre, Materia, Nome do Aluno</p>
+			</div>
+			<div class="panel-footer">
+				<center><a href="">Download<br><span class="glyphicon glyphicon-download"> <b style="text-align: center;"></span></b></a></center>
+			</div>
+		</div>
+	</div> <!-- Div col-md-4-->
 
-			<label>Selecione o curso:</label>
-			<select class="form-control">
-				<option>Todos os cursos</option>
-				<option>Curso 1</option>
-				<option>Curso 2</option>
-			</select><br>
+	<div class="col-md-4">
+		<div class="panel panel-primary"> <!--style="border-radius:50px"-->
+			<div class="panel panel-heading">Relatorio Geral</div>
+			<div class="panel-body">
+				<h4>Formato: .xls (excel)<img src="images/exelIco.png"></h4>
+				<h4>Conteudo do arquivo:</h4>
+				<p>Data, semestre, materia, Nome do Aluno</p>
+			</div>
+			<div class="panel-footer">
+				<center><a href="">Download<br><span class="glyphicon glyphicon-download"> <b style="text-align: center;"></span></b></a></center>
+			</div>
+		</div>
+	</div> <!-- Div col-md-4-->	
 
-			<label>Selecione o semestre:</label>
-			<select class="form-control">
-				<option>Todos os Semestres</option>
-				<option>Semestre 1</option>
-				<option>Semestre 2</option>
-			</select><br>
-
-			<label>Selecione a Materia:</label>
-			<select class="form-control">
-				<option>Todos as Materias</option>
-				<option>Materia 1</option>
-				<option>Materia 2</option>
-			</select><br>
-
-			<button type="submit" class="btn btn-success col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3">Gerar Relatorio</button>
-		</form>
-
-	</div>
 </div>
+
+
 
 <style type="text/css">
 	.form-control{border-color: grey;}
+	img{width: 10%; height: 10%;}
 </style>
 
 @endsection
