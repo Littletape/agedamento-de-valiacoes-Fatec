d3d2a75819b67e16bb5836fdb42c52227af0eb55
Anderson Lima <andersoncardosolima0@gmail.com>
2017-06-11 17:41:37 -0300

    Ajustei design da tela de acesso negado

--- resources/views/acessoNegado.blade.php
+++ resources/views/acessoNegado.blade.php
@@ -1,1 +1,32 @@
-<h1>Acesso negado, você não tem permissão pra acessar essa URL</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+	<script type="text/javascript" src="/js/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/js/modal.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 col-xs-12">
+			<br><br><br>
+			<center>
+				<br>
+				<h1 style="color: #FFD700; font-size: 70px;"><span class="glyphicon glyphicon-alert"></span></h1>
+				<h1 style="color: #FFD700;">Acesso negado, você não tem permissão para acessar essa URL.</h1>
+				<br>
+			</center>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
+
+<style type="text/css">
+	center{background-color: #B22222; border-radius: 40px;}
+</style>
